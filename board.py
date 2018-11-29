75d1e2cc427ebdb03b1d107905e924daec1a1230
sagniksom <sagniksom99@gmail.com>
2018-11-29 18:29:41 -0500

    Update board.py

--- board.py
+++ board.py
@@ -29,20 +29,40 @@ class Board():
                 [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 9],\
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 9],\
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9]]
+        self.visited=[]
+        self.temp=[]
+        for i in range(21):
+            for i in range(20):
+                self.temp+=[0]
+            self.visited.append(self.temp)
         self.indexDict={2: (9, 8), 5: (9, 9), 6: (9, 10), 7: (7, 9)}
-        
+        self.score=0
 
     def index_2d(self, v):
         for i, x in enumerate(self.grid):
             if v in x:
                 return (i, x.index(v))
         return -1
-    def update(self,keyList,indexList):#,SNAKEindex,GHOSTindex,GHOST2index,GHOST3index,GHOST4index):
+
+    def updatePos(self,keyList=[],indexList=[]):#,SNAKEindex,GHOSTindex,GHOST2index,GHOST3index,GHOST4index):
         for i in range(len(indexList)):
             self.indexDict[keyList[i]]=indexList[i]
-        return dictionary
+        return 1
+
+    def updateVisited(self,path=[]):#,SNAKEindex,GHOSTindex,GHOST2index,GHOST3index,GHOST4index):
+        for i in range(len(path)):
+            self.visited[path[i][0]][path[i][1]]+=1
+            if self.visited[path[i][0]][path[i][1]]==1:
+                self.score+=1
+        return self.visited
+
+    def isPellet(self,pos):
+        return self.visited[pos[0]][pos[1]]>0
+
+    def update(self,keyList=[],indexList=[],path=[],score):
+        a=updateVisited(self,path)
+        b=updatePos(self,keyList,indexList)
+        return 1
         
 board=Board()
-print (board.update())
-        
-        
+print (board.updateVisited([(2,10),(3,10),(4,10)]))
