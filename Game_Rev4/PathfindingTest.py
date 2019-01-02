20dc0527bf2b979a5c72d4d1c1cf223d1bb8d539
Daniel Liang <44882266+dkhl65@users.noreply.github.com>
2019-01-01 19:00:59 -0500

    Daniel experiments with pathfinding

---
+++ Game_Rev4/PathfindingTest.py
@@ -0,0 +1,64 @@
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+
+matrix = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0 ,1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 1, 0 ,1, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1 ,1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 1, 0, 0, 0 ,0, 0, 1, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 1, 0 ,1, 1, 1, 0, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, -1, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 1, 0, 1, 0, -2, -3, -4, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+    
+grid = Grid(matrix=matrix)
+
+start = grid.node(9, 15) #15,9
+end = grid.node(8, 7) #7,8
+current = start
+nextNode = current
+
+finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+path, runs = finder.find_path(start, end, grid)
+
+print('operations:', runs, 'path length:', len(path))
+print(grid.grid_str(path=path, start=start, end=end))
+
+#travel the path (doesn't work yet)
+i = 1
+while i <= len(path):
+    for n in grid.neighbors(current):
+        if path and ((n.x, n.y) in path or n in path):
+            if n.y > current.y:
+                print("UP")
+            elif n.x < current.x:
+                print("LEFT")
+            elif n.x > current.x:
+                print("RIGHT")
+            elif n.y < current.y:
+                print("DOWN")
+            nextNode = n
+            break
+    current = nextNode
+    i = i + 1
+        
+
+                
+                            
+        
---
+++ Game_Rev4/README.md
@@ -0,0 +1,1 @@
+This is a revision of the simulator
